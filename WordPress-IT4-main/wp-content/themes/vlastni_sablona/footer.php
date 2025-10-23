ae03382c70ddf7e604794b1a9632975e200e6c1e
Cong Tien Dung Nguyen <Dung.Nguyen@student.souepl.cz>
2025-10-23 08:28:16 +0200

    gmj

--- WordPress-IT4-main/wp-content/themes/vlastni_sablona/footer.php
+++ WordPress-IT4-main/wp-content/themes/vlastni_sablona/footer.php
@@ -1,35 +1,36 @@
-<footer>
-    <div class="wrapper">
-        <div class="wrapper-content">
-            <h2>Zde se nachazí Footer</h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                 Ipsa, suscipit. Quis, pariatur? Sed, architecto ullam ex dolorum
-                  odio accusantium rerum recusandae error totam molestias nemo omnis
-                   nostrum deserunt esse autem?</p>
-            <div class="wrapper-content-social">
-                <a href="#">
+<footer class="footer-ocean">
+  <!-- VLNY (skutečné SVG, animované přes CSS) -->
+  <div class="waves" aria-hidden="true">
+    <!-- zadní, tmavší vlna -->
+    <svg class="wave wave-back" viewBox="0 0 1200 120" preserveAspectRatio="none">
+      <path d="M0,68 C180,28 360,92 540,60 C720,28 900,92 1080,60 C1140,52 1170,56 1200,60 L1200,120 L0,120 Z"></path>
+    </svg>
+    <!-- přední, světlejší vlna -->
+    <svg class="wave wave-front" viewBox="0 0 1200 120" preserveAspectRatio="none">
+      <path d="M0,60 C150,20 300,100 450,60 C600,20 750,100 900,60 C1050,20 1125,40 1200,60 L1200,120 L0,120 Z"></path>
+    </svg>
+  </div>
 
-                </a>
-                <a href="#">
-
-                </a>
-                <a href="#">
-
-                </a>
-                <a href="#">
-
-                </a>
-            </div>    
-        </div>
-        <div class="wrapper-footer">
-            <div class="wrapper-footer-left">
-                <p>© 2024 Všechna práva vyhrazena</p>
-            </div>
-            <div class="wrapper-footer-right">
-                <a href="#">Privacy Policy</a>
-                <a href="#">Terms of Service</a>
-                <a href="#">Contact Us</a>
-            </div>
-        </div>
+  <!-- TVŮJ OBSAH (zůstává, jen je nad vlnami) -->
+  <div class="wrapper content">
+    <div class="wrapper-content">
+      <h1>Zde se nachází footer</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore, eaque similique voluptates
+        cumque cum tenetur, iste hic rerum voluptate unde iure provident aliquam, nulla error tempora
+        voluptatum aut. Cupiditate, soluta.
+      </p>
+      <div class="wrapper-content-social">
+        <a href="a"></a>
+        <a href="a"></a>
+        <a href="a"></a>
+      </div>
     </div>
+  </div>
+
+  <div class="wrapper-footer">
+    <div class="wrapper-footer-left">
+      <!-- sem si dej odkazy / copyright -->
+    </div>
+  </div>
 </footer>
